8d2b70e5394fadd50cde605c86fa4e88cb311da1
Graceless <syerdnaswag@gmail.com>
2022-09-18 02:13:41 +0300

    Added tasks1

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@
 // M = 4; N = 8. -> ""4, 6, 7, 8""
 
 
-string GetNaturalsBetweenWithRecursion(int m, int n)
-{
-  if (m == n) return Convert.ToString(n);
-  return n + ", " + GetNaturalsBetweenWithRecursion(m, n - 1);
-}
+// string GetNaturalsBetweenWithRecursion(int m, int n)
+// {
+//   if (m == n) return Convert.ToString(n);
+//   return n + ", " + GetNaturalsBetweenWithRecursion(m, n - 1);
+// }
 
-System.Console.WriteLine(GetNaturalsBetweenWithRecursion(3, 20));
+// System.Console.WriteLine(GetNaturalsBetweenWithRecursion(3, 20));
 
 
 // Задача 66: Задайте значения M и N. Напишите программу, 
@@ -22,10 +22,10 @@ System.Console.WriteLine(GetNaturalsBetweenWithRecursion(3, 20));
 // M = 4; N = 8. -> 30
 
 int GetSumBetween(int M, int N){
-    if (M == N) return N;
-    return (N + M) + GetSumBetween(M + 1, N - 1);
+    if (M == N) return M;
+    return M + GetSumBetween(M + 1, N);
 }
-System.Console.WriteLine(GetSumBetween(4, 8));
+System.Console.WriteLine(GetSumBetween(1, 15));
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
@@ -33,10 +33,10 @@ System.Console.WriteLine(GetSumBetween(4, 8));
 
 // m = 2, n = 3 -> A(m,n) = 29
 
-int Akkerman(int m, int n){
-    if (m == 0) return n+1;
-    if (n == 0) return Akkerman(m-1, 1);
-    return Akkerman(m - 1, Akkerman(m, n - 1) );
-}
+// int Akkerman(int m, int n){
+//     if (m == 0) return n+1;
+//     if (n == 0) return Akkerman(m-1, 1);
+//     return Akkerman(m - 1, Akkerman(m, n - 1) );
+// }
 
-System.Console.WriteLine(Akkerman(3, 2));
+// System.Console.WriteLine(Akkerman(3, 2));
